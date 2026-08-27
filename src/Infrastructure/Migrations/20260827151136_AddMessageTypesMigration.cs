a930e2fee5e4b850a1d4cdfdc9d1041d5d51ee67
Sahiam1810 <222233860+Sahiam1810@users.noreply.github.com>
2026-08-27 10:12:26 -0500

    feat: :sparkles: add message types migration - Created MESSAGE_TYPES table with necessary fields and relationships for managing message types.

---
+++ src/Infrastructure/Migrations/20260827151136_AddMessageTypesMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMessageTypesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MESSAGE_TYPES",
+                columns: table => new
+                {
+                    MESSAGE_TYPES_ID = table.Column<string>(type: "VARCHAR2(36)", nullable: false),
+                    NAME_TYPE = table.Column<string>(type: "VARCHAR2(50)", maxLength: 50, nullable: false),
+                    CREATED_AT = table.Column<DateTime>(type: "TIMESTAMP", nullable: false),
+                    UPDATED_AT = table.Column<DateTime>(type: "TIMESTAMP", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MESSAGE_TYPES", x => x.MESSAGE_TYPES_ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MESSAGE_TYPES");
+        }
+    }
+}
