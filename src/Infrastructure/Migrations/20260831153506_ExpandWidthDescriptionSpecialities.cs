caa98a53484bcc4da671ef307c1f0610ecd83c45
Sahiam1810 <222233860+Sahiam1810@users.noreply.github.com>
2026-08-31 10:36:02 -0500

    fix: :bug: feat(migrations): expand width of NAME and DESCRIPTION columns in SPECIALTIES table  - Increased the maximum length of the NAME column from 30 to 120 characters. - Increased the maximum length of the DESCRIPTION column from 30 to 120 characters.
    feat(migrations): expand width of NAME and DESCRIPTION columns in SPECIALTIES table  - Increased the maximum length of the NAME column from 30 to 120 characters. - Increased the maximum length of the DESCRIPTION column from 30 to 120 characters.

---
+++ src/Infrastructure/Migrations/20260831153506_ExpandWidthDescriptionSpecialities.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpandWidthDescriptionSpecialities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NAME",
+                table: "SPECIALTIES",
+                type: "VARCHAR2(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR2(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DESCRIPTION",
+                table: "SPECIALTIES",
+                type: "VARCHAR2(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR2(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NAME",
+                table: "SPECIALTIES",
+                type: "VARCHAR2(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR2(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DESCRIPTION",
+                table: "SPECIALTIES",
+                type: "VARCHAR2(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR2(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+        }
+    }
+}
